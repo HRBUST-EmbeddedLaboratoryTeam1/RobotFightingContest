a4b026a8377f347ae659000239e5397e35452835
DemonHxw <56249421+DemonHxw@users.noreply.github.com>
2020-09-29 13:47:46 +0800

    Add files via upload
    20200929 13: 47
    能上台 显示器出问题，白屏，侧面能看清字符

--- src/main.c
+++ src/main.c
@@ -6,17 +6,26 @@ const int GEAR_ANGLE_6 = 485;	//6号舵机平衡位置角度
 const int GEAR_ANGLE_4 = 511;	//4号舵机平衡位置角度
 const int GEAR_ANGLE_5 = 515;	//5号舵机平衡位置角度
 
+const int GEAR_ANGLE_INIT = 400; //初始化舵机变换角度
+
 const int SPEED_GEAR = 350;	//舵机速度
 
 void InitSys();	//初始化系统
 void InitGear();	//初始化舵机
 void InitMotor();	//初始化电机
+
+void InitForepaw(); //初始化前爪
+void InitHindpaw(); //初始化后爪 
+
 void InitClaw();	//初始化爪子
 
 void FirstUpStage();	//第一次上台
 
 void DebugGearbalan();	//测试爪子平衡位置
 void DebugInfraredSensor(int,int,int); //测试红外传感器
+
+
+void MoveForword(int,int); //向前走
 
 /**
  * Title: Init()
@@ -77,19 +86,58 @@ void InitMotor()
 }
 
 /**
- * Title: InitMClaw()
+ * Title: InitForepaw()
  * Return: None
- * Author: Ben
+ * Author: Altria
+ * Descr: 初始化前爪
+ * LastBuild: 20200929
+ */
+void InitForepaw() {
+	UP_CDS_SetAngle(3, GEAR_ANGLE_3 + GEAR_ANGLE_INIT, SPEED_GEAR);
+	UP_CDS_SetAngle(6, GEAR_ANGLE_6 - GEAR_ANGLE_INIT, SPEED_GEAR);
+}
+
+/**
+ * Title: InitHindpaw()
+ * Return: None
+ * Author: Altria
+ * Descr: 初始化后爪
+ * LastBuild: 20200929
+ */
+void InitHindpaw() {
+	UP_CDS_SetAngle(4, GEAR_ANGLE_4 + GEAR_ANGLE_INIT, SPEED_GEAR);
+	UP_CDS_SetAngle(5, GEAR_ANGLE_5 - GEAR_ANGLE_INIT, SPEED_GEAR);
+}
+
+/**
+ * Title: InitClaw()
+ * Return: None
+ * Author: Altria
  * Descr: 设置爪子初始状态
- * LastBuild: 20200928
+ * LastBuild: 20200929
  */
 void InitClaw()
 {
-	UP_CDS_SetAngle(3, 511+450, SPEED_GEAR);
-	UP_CDS_SetAngle(6, 511-450, SPEED_GEAR);
-	UP_CDS_SetAngle(4, 511+450, SPEED_GEAR);
-	UP_CDS_SetAngle(5, 511-450, SPEED_GEAR);
+	InitForepaw();
+	InitHindpaw();
 	UP_delay_ms(2000);
+}
+
+/**
+ * Title: MoveForword(int,int)
+ * args: spped - 运行速度
+ * args: time - 运行时间
+ * Return: None
+ * Author: Altria
+ * Descr: 以速度spee向前行驶time时间
+ * LastBuild: 20200928
+ */
+void MoveForword(int speed, int time) {
+	UP_CDS_SetSpeed(1, -speed);
+	UP_CDS_SetSpeed(2, speed);
+	UP_delay_ms(time);
+	UP_CDS_SetSpeed(1, 0);
+	UP_CDS_SetSpeed(2, 0);
 }
 
 /**
@@ -122,11 +170,28 @@ void DebugInfraredSensor(int x,int y,int id) {
 }
 
 /**
- * Title: FirstUpStage()
+ * Title: DebugAllInfraredSensor(int)
  * Return: None
  * Author: Ben
- * Descr: 第一次上台
+ * Descr: 调试全部红外传感器
  * LastBuild: 20200928
+ */
+void DebugAllInfraredSensor()
+{
+	while(1) {
+		DebugInfraredSensor(1,1,0);
+		DebugInfraredSensor(1,2,1);
+		DebugInfraredSensor(1,3,3);
+		UP_delay_ms(500);
+	}
+}
+
+/**
+ * Title: FirstUpStage()
+ * Return: None
+ * Author: Altria
+ * Descr: 第一次上台
+ * LastBuild: 20200929
  */
 void FirstUpStage()
 {
@@ -136,84 +201,40 @@ void FirstUpStage()
 	UP_delay_ms(1500);
 	
 	//后轮启动
-	UP_CDS_SetSpeed(1, -SPEED_GEAR);
-	UP_CDS_SetSpeed(2, SPEED_GEAR);
-	UP_delay_ms(800);
-	UP_CDS_SetSpeed(1, 0);
-	UP_CDS_SetSpeed(2, 0);
+	MoveForword(SPEED_GEAR,700);
+	UP_delay_ms(1500);
+
+	//前爪复位
+	InitForepaw();
+	UP_delay_ms(1500);
+
+	//后爪撑地
+	UP_CDS_SetAngle(4, GEAR_ANGLE_4 - 120, SPEED_GEAR);
+	UP_CDS_SetAngle(5, GEAR_ANGLE_5 + 120, SPEED_GEAR);
+	UP_delay_ms(1500);
+
+	//前轮启动
+	MoveForword(SPEED_GEAR,700);
+	UP_delay_ms(1500);
+
+	//后爪复位
+	InitHindpaw();
+	UP_delay_ms(1500);
 }
 
 int main(void)
 {	
 	Init();
 
-	UP_LCD_ClearScreen();
-	// while(1) {
-	// 	DebugInfraredSensor(1,1,0);
-	// 	DebugInfraredSensor(1,2,1);
-	// 	DebugInfraredSensor(1,3,3);
-	// 	UP_delay_ms(500);
-	// }
+	// UP_LCD_ClearScreen();
 	
 	// DebugGearBalan();	//测试舵机平衡位置
 
-	/*****************************************************软启动函数********************************************************/
+	/*******************************************软启动函数**************************************************/
 
 	//第一次上台
 	FirstUpStage();
 
-	// UP_CDS_SetAngle(3, GEAR_ANGLE-160, 300);				//设置舵机转角(0~1023对应0~300°)，速度（0~1023）
-	// UP_CDS_SetAngle(6, GEAR_ANGLE+130, 300);				//设置舵机转角(0~1023对应0~300°)，速度（0~1023）
 	UP_delay_ms(1000);								
 	UP_LCD_ClearLine(1);							//清除第2行字符
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
-//		UP_CDS_SetAngle(4, 200, 500);				//设置舵机转角(0~1023对应0~300°)，速度（0~1023）
-//		UP_delay_ms(1000);								
-//		UP_LCD_ClearLine(1);							//清除第2行字符
-//		
-//		UP_CDS_SetAngle(5, 200, 500);				//设置舵机转角(0~1023对应0~300°)，速度（0~1023）
-//		UP_delay_ms(1000);								
-//		UP_LCD_ClearLine(1);							//清除第2行字符
-//		
-//		UP_CDS_SetAngle(6, 200, 500);				//设置舵机转角(0~1023对应0~300°)，速度（0~1023）
-//		UP_delay_ms(1000);								
-//		UP_LCD_ClearLine(1);							//清除第2行字符
-		
-		
-//		printf("\nAngle:235");							
-//		UP_CDS_SetAngle(4, 800, 500);	
-//		UP_delay_ms(2000); 								//延时2s
-//		UP_LCD_ClearScreen();							//清屏
-//		
-//		
-
-//		
-//		printf("Speed:+800");	
-//		UP_CDS_SetSpeed(1, 800);					//设置电机模式下的速度，范围-1023~+1023	
-//		UP_delay_ms(1000);								//延时1s
-//		UP_LCD_ClearLine(1);							//清除第2行字符
-//		
-//		printf("\nSpeed:-800");	
-//		UP_CDS_SetSpeed(2, 800);					//设置电机模式下的速度，范围-1023~+1023
-//		UP_delay_ms(1000); 								//延时1s		
-//		UP_LCD_ClearLine(1);							//清除第2行字符
-//		
-//		printf("\nStop");
-//		UP_CDS_SetSpeed(1, 0);						//停止
-//		UP_delay_ms(1000); 								//延时2s
-//		UP_LCD_ClearScreen();							//清屏 
 }
